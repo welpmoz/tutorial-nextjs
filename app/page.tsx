513e7c6df860f993c03a470f476de612ee9d128b
roswell <roswell_pando@outlook.es>
2023-11-03 10:16:47 -0500

    optimizando las imagenes de la web

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import styles from '@/app/ui/home.module.css';
 import AcmeLogo from '@/app/ui/acme-logo';
 import Link from 'next/link';
 import { lusitana } from './ui/invoices/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -27,6 +28,23 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            // Especificar el ancho y alto para un
+            // redimensionamiento que mantenga el aspecto de la imagen
+            width={500}
+            height={380}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
+            
         </div>
       </div>
     </main>
