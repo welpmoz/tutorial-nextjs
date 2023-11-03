4ee210d682e8100538471887a3abe1e764d570b4
roswell <roswell_pando@outlook.es>
2023-11-03 09:47:56 -0500

    usando modulos de css

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import styles from '@/app/ui/home.module.css';
 import AcmeLogo from '@/app/ui/acme-logo';
 
 import Link from 'next/link';
@@ -7,7 +8,7 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+      <div className={styles.shape}>
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
